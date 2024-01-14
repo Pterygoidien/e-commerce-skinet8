e735319b041d7fc4f9d41c23096d9ff4257da626
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2024-01-15 00:11:59 +0100

    Client| Layout module, child of core module

---
+++ client/src/app/core/layout/app-layout.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+@NgModule({
+  declarations: [NavBarComponent],
+  exports: [NavBarComponent],
+})
+export class AppLayoutModule {}
