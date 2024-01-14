0a8a1cb15d7fb125a73a447f151e6f777303b083
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2024-01-15 00:12:31 +0100

    Client| Import CoreModule, HttpClientModule into AppModule

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+import { AppCoreModule } from './core/app-core.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AppCoreModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
