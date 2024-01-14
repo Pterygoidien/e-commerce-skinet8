2ceeccac16928c26581bf0714a68dba7b754acb4
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2024-01-15 00:13:45 +0100

    Client| App component (app-root) : implement OnInit interface to AppComponent class, use of HttpClientModule to fetch data to ASP.NET API

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Skinet';
+
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (error) => {
+        console.log(error);
+      },
+      complete: () => {
+        console.log('request completed');
+        console.log('extra statement');
+      },
+    });
+  }
 }
