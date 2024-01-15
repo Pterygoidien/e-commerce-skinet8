7acdb4bcc6e98f7a8c8f437e25bda89104ed0abf
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2024-01-15 02:23:49 +0100

    Client| App-root : remove OnInit methods (refactoring : move to shop Service)

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,21 +6,7 @@ import { Observable } from 'rxjs';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  title = 'Skinet';
-
-  constructor(private http: HttpClient) {}
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe({
-      next: (response) => {
-        console.log(response);
-      },
-      error: (error) => {
-        console.log(error);
-      },
-      complete: () => {
-        console.log('request completed');
-        console.log('extra statement');
-      },
-    });
+    throw new Error('Method not implemented.');
   }
 }
