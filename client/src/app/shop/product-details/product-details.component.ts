f7976c5704d475385ebd7531777da464e4d735f5
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2024-01-15 19:41:57 +0100

    Client| Shop : add product-details domain logic in ProductDetailsComponent class, calling ShopService and ActivatedRoute to retrieve data (url params : id), implement OnInit, load product details on init

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ShopService } from '../shop.service';
+import { ActivatedRoute } from '@angular/router';
+import { IProduct } from 'src/app/shared/models/product';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.scss']
+  styleUrls: ['./product-details.component.scss'],
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+  product?: IProduct;
+  constructor(
+    private shopService: ShopService,
+    private activateRoute: ActivatedRoute
+  ) {}
 
+  ngOnInit(): void {
+    this.loadProduct();
+  }
+
+  loadProduct(): void {
+    const id: string | null = this.activateRoute.snapshot.paramMap.get('id');
+    if (id)
+      this.shopService.getProduct(+id).subscribe({
+        next: (product) => (this.product = product),
+        error: (err) => console.log(err),
+      });
+  }
 }
