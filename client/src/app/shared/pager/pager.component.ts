8d29a75cf4169988c02e033e0bce0b0a0dcd77c7
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2024-01-15 15:11:37 +0100

    Client| Shared : pager-component domain-logic : declare Input parameters (props), output parameter with EventEmitter for pageChange event

--- client/src/app/shared/pager/pager.component.ts
+++ client/src/app/shared/pager/pager.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { PageChangedEvent } from 'ngx-bootstrap/pagination';
 
 @Component({
   selector: 'app-pager',
   templateUrl: './pager.component.html',
-  styleUrls: ['./pager.component.scss']
+  styleUrls: ['./pager.component.scss'],
 })
 export class PagerComponent {
+  @Input() totalCount?: number;
+  @Input() pageSize?: number;
+  @Output() pageChanged = new EventEmitter<number>();
 
+  onPagerChanged(event: PageChangedEvent) {
+    this.pageChanged.emit(event.page);
+  }
 }
