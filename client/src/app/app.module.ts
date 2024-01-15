c30404af0f7ff0a88cff1a716a3d952a9cd0de2f
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2024-01-15 20:55:06 +0100

    Client| App-root : register custom ErrorInterceptor (HTTP Interceptor) in AppModule providers

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppCoreModule } from './core/app-core.module';
 import { HomeModule } from './home/home.module';
+import { ErrorInterceptor } from './core/interceptors/error.interceptor';
 
 @NgModule({
   declarations: [AppComponent],
@@ -18,7 +19,13 @@ import { HomeModule } from './home/home.module';
     HttpClientModule,
     HomeModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
