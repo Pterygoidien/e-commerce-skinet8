44a5df825cc30c4345c742d1ab097429a1bb1b7f
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2024-01-15 14:40:30 +0100

    Client| Shared : import PaginationModule from ngx-bootstrap, declare PagingHeader and Pager components, export them to make them available

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { PagingHeaderComponent } from './paging-header/paging-header.component';
+import { PagerComponent } from './pager/pager.component';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
-  exports: [],
+  declarations: [PagingHeaderComponent, PagerComponent],
+  imports: [CommonModule, PaginationModule.forRoot()],
+  exports: [PaginationModule, PagingHeaderComponent],
   providers: [],
 })
 export class SharedModule {}
