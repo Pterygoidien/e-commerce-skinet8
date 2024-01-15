a7ec292aa08cefb25898e2a53231b2c4aeb816bd
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2024-01-15 14:37:06 +0100

    Client| Shared : New pager component (pagination) with ngx-bootstrap

---
+++ client/src/app/shared/pager/pager.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pager',
+  templateUrl: './pager.component.html',
+  styleUrls: ['./pager.component.scss']
+})
+export class PagerComponent {
+
+}
