2b760650b17545058c93edcb30dc40f88779b5c3
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2024-01-15 14:35:46 +0100

    Client| App-root : remove onInit implementation (not needed anymore)

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -5,8 +5,4 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
-}
+export class AppComponent {}
