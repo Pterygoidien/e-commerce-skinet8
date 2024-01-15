d61954c841c2f7daa40efaaad77c0ec854fd8916
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2024-01-15 19:43:51 +0100

    Client| Shop : add Shop router (child of main router), add specific routes for /shop/ endpoints

---
+++ client/src/app/shop/shop-routing.module.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ShopComponent } from './shop.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+
+const routes: Routes = [
+  { path: '', component: ShopComponent },
+  { path: ':id', component: ProductDetailsComponent },
+];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class ShopRoutingModule {}
