206dfb41f99d5b1369b1ce2da1d8ba763a140eca
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2024-01-15 19:44:27 +0100

    Client| Shop : register ShopRoutingModule, remove main router, remove exports that have become unnecessary

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -4,10 +4,12 @@ import { ShopComponent } from './shop.component';
 import { ProductItemComponent } from './product-item/product-item.component';
 import { SharedModule } from '../shared/shared.module';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { RouterModule } from '@angular/router';
+import { ShopRoutingModule } from './shop-routing.module';
 
 @NgModule({
   declarations: [ShopComponent, ProductItemComponent, ProductDetailsComponent],
-  imports: [CommonModule, SharedModule],
-  exports: [ShopComponent],
+  imports: [CommonModule, SharedModule, ShopRoutingModule],
+  exports: [],
 })
 export class ShopModule {}
