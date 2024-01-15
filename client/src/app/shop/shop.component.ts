636d0a73c7254d11ed42d6a02f8ff7561caba371
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2024-01-15 14:39:28 +0100

    Client| Shop : refactoring shop component with ShopParams DTO : instantiated a new ShopParams, pass the values, refactor methods to call services with ShopParams instance

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -4,6 +4,8 @@ import { ShopService } from './shop.service';
 import { IBrand } from '../shared/models/brand';
 import { IType } from '../shared/models/type';
 import { IPagination } from '../shared/models/pagination';
+import { ShopParams } from '../shared/models/shopParams';
+import { PageChangedEvent } from 'ngx-bootstrap/pagination';
 
 @Component({
   selector: 'app-shop',
@@ -14,6 +16,13 @@ export class ShopComponent implements OnInit {
   products: IProduct[] = [];
   brands: IBrand[] = [];
   types: IType[] = [];
+  shopParams: ShopParams = new ShopParams();
+  sortOptions = [
+    { name: 'Alphabetical', value: 'name' },
+    { name: 'Price: Low to High', value: 'priceAsc' },
+    { name: 'Price: High to Low', value: 'priceDesc' },
+  ];
+  totalCount = 0;
 
   constructor(private shopService: ShopService) {}
 
@@ -24,16 +33,18 @@ export class ShopComponent implements OnInit {
   }
 
   getProduts(): void {
-    this.shopService.getProducts().subscribe({
+    this.shopService.getProducts(this.shopParams).subscribe({
       next: (response: IPagination<IProduct[]>) => {
         this.products = response.data;
+        this.shopParams.pageNumber = response.pageIndex;
+        this.shopParams.pageSize = response.pageSize;
+        this.totalCount = response.count;
       },
       error: (error) => {
         console.log(error);
       },
       complete: () => {
-        console.log('request completed');
-        console.log('extra statement');
+        console.log('complete');
       },
     });
   }
@@ -41,21 +52,46 @@ export class ShopComponent implements OnInit {
   getBrands(): void {
     this.shopService.getBrands().subscribe({
       next: (response: IBrand[]) => {
-        this.brands = response;
+        this.brands = [{ id: 0, name: 'All' }, ...response];
       },
       error: (error) => {
         console.log(error);
       },
     });
   }
+
   getTypes(): void {
     this.shopService.getTypes().subscribe({
       next: (response: any) => {
-        this.types = response;
+        this.types = [{ id: 0, name: 'All' }, ...response];
       },
       error: (error) => {
         console.log(error);
       },
     });
+  }
+
+  onBrandSelected(brandId: number): void {
+    this.shopParams.brandId = brandId;
+    this.getProduts();
+  }
+
+  onTypeSelected(typeId: number): void {
+    this.shopParams.typeId = typeId;
+    this.getProduts();
+  }
+
+  onSortSelected(event: Event): void {
+    this.shopParams.sort = (event.target as HTMLSelectElement).value;
+    this.getProduts();
+  }
+
+  onPageChanged(event: PageChangedEvent): void {
+    if (this.shopParams.pageNumber !== event.page) {
+      console.log(typeof event);
+      console.log(typeof event.page);
+      this.shopParams.pageNumber = event.page;
+      this.getProduts();
+    }
   }
 }
